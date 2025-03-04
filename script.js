8a49d0f742191c0fd8734d71e714be1c8fc4c40e
Tasnimmou11 <ummemou44@gmail.com>
2025-03-04 22:45:22 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+//of array
+const arr = ["a","b","c","d",1,2,3]
+    for(item of arr){
+    console.log(item);
+
+}
+
+const str = "HELLO WORLD"
+for(item of str){
+    console.log(item);
+}
+
+
+//in object
+const obj ={
+    name: "Tasnim Mou",
+    age: 18,
+    address: "Dhaka"
+}
+for(item in obj){
+    console.log(item); 
+}
