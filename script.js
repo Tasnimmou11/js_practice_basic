f23e13f1cf6c760d542e4d5fafa9e70b8f1efb41
Tasnimmou11 <ummemou44@gmail.com>
2025-03-05 23:30:39 +0600

    function

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 //of array
-const arr = ["a","b","c","d",1,2,3]
-    for(item of arr){
-    console.log(item);
-
+const arr = ["a", "b", "c", "d", 1, 2, 3];
+for (item of arr) {
+  console.log(item);
 }
 
-const str = "HELLO WORLD"
-for(item of str){
-    console.log(item);
+const str = "HELLO WORLD";
+for (item of str) {
+  console.log(item);
 }
-
 
 //in object
-const obj ={
-    name: "Tasnim Mou",
-    age: 18,
-    address: "Dhaka"
+const obj = {
+  name: "Tasnim Mou",
+  age: 18,
+  address: "Dhaka",
+};
+for (item in obj) {
+  console.log(item);
 }
-for(item in obj){
-    console.log(item); 
+
+///function
+function showMessage() {
+  console.log("Hello everyone!");
 }
+
+showMessage();
+showMessage();
+
+function showMessage() {
+    let message = "Hello, I'm JavaScript!"; // local variable
+  
+   console.log( message );
+  }
+  
+  showMessage(); // Hello, I'm JavaScript!
+  
